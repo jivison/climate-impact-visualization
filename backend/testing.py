aed35e71aa36ff5ad9d2bc306b9d3198e03dcdf1
John Ivison <john@ivison.org>
2019-10-12 16:21:35 -0700

    Add gitignore

---
+++ backend/testing.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
